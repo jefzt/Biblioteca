d3bd1d9f1a97a17f35d3f527cc65b357c10c98ba
ValentinaCamachoB <Valencamacho777@hotmail.com>
2026-03-02 22:16:22 -0500

    Clase usuario atributos

---
+++ Usuario.java
@@ -0,0 +1,39 @@
+package Biblioteca;
+
+public class Usuario {
+    private String documento;
+    private String nombreCompleto;
+    private int rol;
+    private String contraseña;
+
+    public Usuario(String documento, String nombreCompleto, int rol, String contraseña) {
+        this.documento = documento;
+        this.nombreCompleto = nombreCompleto;
+        this.rol = rol;
+        this.contraseña = contraseña;
+    }
+
+    public String getDocumento() {
+        return documento;
+    }
+
+    public String getNombreCompleto() {
+        return nombreCompleto;
+    }
+
+    public int getRol() {
+        return rol;
+    }
+
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    public boolean puedeRegistrarPrestamos() {
+        return rol == 1 || rol == 2;
+    }
+
+    public boolean esAdministrador() {
+        return rol == 1;
+    }
+}
