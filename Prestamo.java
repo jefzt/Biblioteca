4fff188a36c90c3f5d2959c1e8f8d46a2c9297d8
jefzt <jefrylv10@gmail.com>
2026-03-22 18:15:26 -0500

    Update Prestamo.java

--- Prestamo.java
+++ Prestamo.java
@@ -15,4 +15,24 @@ public class Prestamo {
         this.activo           = true;
     }
 
- 
+    public String    getDocumentoUsuario() { return documentoUsuario; }
+    public String    getIsbnLibro()        { return isbnLibro;        }
+    public LocalDate getFechaPrestamo()    { return fechaPrestamo;    }
+    public boolean   isActivo()            { return activo;           }
+
+    public void registrarDevolucion() {
+        this.activo          = false;
+        this.fechaDevolucion = LocalDate.now();
+    }
+
+    @Override
+    public String toString() {
+        return "-------------------------------"
+             + "\n  Usuario  : " + documentoUsuario
+             + "\n  ISBN     : " + isbnLibro
+             + "\n  Prestado : " + fechaPrestamo
+             + "\n  Devuelto : " + (fechaDevolucion != null ? fechaDevolucion : "Pendiente")
+             + "\n  Estado   : " + (activo ? "ACTIVO" : "DEVUELTO")
+             + "\n-------------------------------";
+    }
+}
