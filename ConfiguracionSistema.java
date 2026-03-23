682b56914e4e9de17896cc3a7cdc4db38ffd3b0e
ValentinaCamachoB <Valencamacho777@hotmail.com>
2026-03-23 12:00:29 -0500

    Constantes: nombre, version, maxPrestamos, maxIntentosLogin

--- ConfiguracionSistema.java
+++ ConfiguracionSistema.java
@@ -1,4 +1,9 @@
 public class ConfiguracionSistema {
+
+    private static final String NOMBRE_BIBLIOTECA  = "Biblioteca Municipal SENA";
+    private static final String VERSION_SISTEMA    = "1.0";
+    private static final int    MAX_PRESTAMOS      = 3;
+    private static final int    MAX_INTENTOS_LOGIN = 3;
     
     private static ConfiguracionSistema unicaInstancia = null;
 
@@ -9,5 +14,20 @@ public class ConfiguracionSistema {
             unicaInstancia = new ConfiguracionSistema();
         }
         return unicaInstancia;
+    }
+
+    public String getNombreBiblioteca() { return NOMBRE_BIBLIOTECA;  }
+    public String getVersionSistema()   { return VERSION_SISTEMA;    }
+    public int    getMaxPrestamos()     { return MAX_PRESTAMOS;      }
+    public int    getMaxIntentosLogin() { return MAX_INTENTOS_LOGIN; }
+
+    @Override
+    public String toString() {
+        return "\n----- Configuracion del Sistema -----"
+             + "\n  Nombre    : " + NOMBRE_BIBLIOTECA
+             + "\n  Version   : " + VERSION_SISTEMA
+             + "\n  Prestamos : " + MAX_PRESTAMOS
+             + "\n  Intentos  : " + MAX_INTENTOS_LOGIN
+             + "\n-------------------------------------";
     }
 }
