dd13ecc73505d3a9fad93a78bce6270e44e2d69f
jefzt <jefrylv10@gmail.com>
2026-03-24 08:40:46 -0500

    Seguridad.java

--- Seguridad.java
+++ Seguridad.java
@@ -1,1 +1,13 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
+
+public class Seguridad {
+
+    private ArrayList<Libro>    libros;
+    private ArrayList<Usuario>  usuarios;
+    private ArrayList<Prestamo> prestamos;
+    private Usuario             usuarioActivo;
+    private Scanner             scanner;
 
